4f1f2633c9f5261de11856e2458bbe6f9acd1b80
ReinaldoFernandezC <101837525+ReinaldoFernandezC@users.noreply.github.com>
2022-07-01 05:39:38 -0400

    Delete patitas directory

--- patitas/articulos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from articulos.views import lista_articulos,detalle_articulo
-
-urlpatterns=[
-    path('lista_articulos',lista_articulos,name='lista_articulos'),
-    path('detalle_articulo/<id>',detalle_articulo, name='detalle_articulo'),
-]
--- patitas/core/forms.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from django import forms
-from django.forms import ModelForm
-from .models import Articulo
-
-class ArticulosForm(ModelForm):
-    class Meta:
-        model=Articulo
-        fields=['idArticulo','precio','nombreProducto','especie','categoria']
