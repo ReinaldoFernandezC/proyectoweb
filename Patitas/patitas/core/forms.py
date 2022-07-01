847eefb83931e689d223ff48e286f9025d15ce59
ReinaldoFernandezC <101837525+ReinaldoFernandezC@users.noreply.github.com>
2022-07-01 05:29:43 -0400

    Delete Patitas/patitas directory

--- Patitas/patitas/core/forms.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from django import forms
-from django.forms import ModelForm
-from .models import articulo
-
-class articuloForm(ModelForm):
-    class Meta:
-        model=articulo
-        fields=['idArticulo','Precio','especie','categoria']
--- Patitas/patitas/patitas/views.py
+++
@@ -1,3 +0,0 @@
-from django.http import HttpResponse
-def bienvenida(request):
-    return HttpResponse("bienvenida")
